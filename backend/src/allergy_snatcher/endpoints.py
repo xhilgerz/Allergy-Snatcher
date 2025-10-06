5b711bd9efc65b4a6ee6c4ce36079768d8487a2f
Bionix <SCBionicle@gmail.com>
2025-10-06 10:25:32 -0500

    implement first http method with sample data

--- backend/src/allergy_snatcher/endpoints.py
+++ backend/src/allergy_snatcher/endpoints.py
@@ -4,3 +4,9 @@ from flask import Flask, Blueprint, jsonify
 routes = Blueprint('routes', __name__)
 
 
+@routes.route("/api/categories/", methods=['GET'])
+def get_categories():
+
+    # Todo: Get categories from database
+
+    return jsonify(["Sample", "Glutten-free"])
