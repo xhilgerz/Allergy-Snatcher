14a28c0af5e354fa9b35f19bdb2ba26bffc2a2f9
Bionix <SCBionicle@gmail.com>
2025-10-06 10:32:33 -0500

    add endpoint to config

--- backend/src/allergy_snatcher/__main__.py
+++ backend/src/allergy_snatcher/__main__.py
@@ -8,7 +8,9 @@ def main() -> None:
     oauth = OAuth(app)
     
      #app.config.from_object('allergy_snatcher.config.Config') 
-
+    
+    from allergy_snatcher.endpoints import routes
+    app.register_blueprint(routes)
     app.run(debug=True, host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
