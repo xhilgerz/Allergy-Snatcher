f7c30a4e52c87320b08eba257a6c9f705d45e5ad
Bionix <SCBionicle@gmail.com>
2025-11-03 15:37:54 -0600

    implement ASGI middlewhere

--- backend/src/allergy_snatcher/wsgi.py
+++ backend/src/allergy_snatcher/wsgi.py
@@ -1,3 +1,8 @@
+from a2wsgi import ASGIMiddleware
 from allergy_snatcher.__main__ import main
 
-app = main()
+# main() returns the raw WSGI app
+wsgi_app = main()
+
+# Wrap the WSGI app to make it ASGI-compatible for Uvicorn
+app = ASGIMiddleware(wsgi_app)
