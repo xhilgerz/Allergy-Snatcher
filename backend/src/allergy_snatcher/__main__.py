fe94bcb78ed445e030efed9a29783ef7a5b1b71c
Bionix <SCBionicle@gmail.com>
2025-11-02 20:34:43 -0600

    add configuration to backend for auth and secret key

--- backend/src/allergy_snatcher/__main__.py
+++ backend/src/allergy_snatcher/__main__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from allergy_snatcher.models.database import db
 from allergy_snatcher.routes.auth import init_app as auth_init_app
 
@@ -6,7 +7,51 @@ def main() -> None:
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://myuser:mypassword@db:3306/mydatabase'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'supersecretkey' # Change this in production
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', None) # Change this in production
+    if not app.config['SECRET_KEY']:
+        raise ValueError('SECRET_KEY is not set')
+    
+
+    oauth_providers = {}
+
+    # Google
+    if 'OAUTH_GOOGLE_CLIENT_ID' in os.environ:
+        oauth_providers['google'] = {
+            'client_id': os.environ.get('OAUTH_GOOGLE_CLIENT_ID'),
+            'client_secret': os.environ.get('OAUTH_GOOGLE_CLIENT_SECRET'),
+            'server_metadata_url': 'https://accounts.google.com/.well-known/openid-configuration',
+            'client_kwargs': {
+                'scope': 'openid email profile'
+            }
+        }
+
+    # GitHub
+    if 'OAUTH_GITHUB_CLIENT_ID' in os.environ:
+        oauth_providers['github'] = {
+            'client_id': os.environ.get('OAUTH_GITHUB_CLIENT_ID'),
+            'client_secret': os.environ.get('OAUTH_GITHUB_CLIENT_SECRET'),
+            'api_base_url': 'https://api.github.com/',
+            'userinfo_endpoint': 'https://api.github.com/user',
+            'authorize_url': 'https://github.com/login/oauth/authorize',
+            'access_token_url': 'https://github.com/login/oauth/access_token',
+            'client_kwargs': {
+                'scope': 'user:email'
+            }
+        }
+
+
+    # Authentik
+    if 'OAUTH_AUTHENTIK_CLIENT_ID' in os.environ:
+        oauth_providers['authentik'] = {
+            'client_id': os.environ.get('OAUTH_AUTHENTIK_CLIENT_ID'),
+            'client_secret': os.environ.get('OAUTH_AUTHENTIK_CLIENT_SECRET'),
+            'server_metadata_url': os.environ.get('OAUTH_AUTHENTIK_SERVER_METADATA_URL'),
+            'client_kwargs': {
+                'scope': 'openid email profile'
+            }
+        }
+
+    app.config['OAUTH_PROVIDERS'] = oauth_providers
 
     db.init_app(app)
     auth_init_app(app)
